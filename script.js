e8adc68386a65f2e26abe2c3a6f3c8e70bfb1155
Abhee Krishna <abheekrishna17@gmail.com>
2025-08-27 06:11:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('contactForm');
+  const message = document.getElementById('formMessage');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    message.style.display = 'block'; 
+    form.reset(); 
+  });
+});
